1786c3462999ce7c68629456b06a3d00c6d5d706
florisusuri <florisusuri23@gmail.com>
2026-01-28 14:44:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "The main goal of this marketing project is to grow Zhuri sh.p.k. through smart and effective marketing strategies.";
+}
